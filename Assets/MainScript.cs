51f6a26b0de9af863f6db5637c6375708cb94a11
derp derp <workingbahm@yahoo.com.sg>
2017-11-20 00:53:15 -0800

    Initial commit before I change everything.

---
+++ Assets/MainScript.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour 
+{
+    public Texture2D NoiseSource;
+    public ComputeShader Computer;
+    public Material Mat;
+    public int PointsCount;
+
+    public int Lifespan;
+    public float Dampener;
+
+    private ComputeBuffer _fixedDataBuffer;
+    private const int FixedDataStride = sizeof(float) * 2; // Start Location 
+
+    private ComputeBuffer _variableDataBuffer;
+    private const int VariableDataStride = sizeof(float) * 2 // Current Location
+                                                + sizeof(float) // Velocity
+                                                + sizeof(int); // Life Remaining
+
+    private int _computeKernel;
+    private int _groupsCount;
+
+    public struct FixedSliceData
+    {
+        public Vector2 StartLocation;
+    }
+
+    public struct VariableSliceData
+    {
+        public Vector2 CurrentLocation;
+        public float Velocity; 
+        public int LifeRemaining;
+    }
+
+    private void Start()
+    {
+        _computeKernel = Computer.FindKernel("ComputeSlice");
+        _fixedDataBuffer = GetFixedDataBuffer();
+        _variableDataBuffer = GetVariableDataBuffer();
+        _groupsCount = Mathf.CeilToInt((float)PointsCount / 128);
+    }
+
+    private ComputeBuffer GetVariableDataBuffer()
+    {
+        ComputeBuffer ret = new ComputeBuffer(PointsCount, VariableDataStride);
+        VariableSliceData[] data = new VariableSliceData[PointsCount];
+        for (int i = 0; i < PointsCount; i++)
+        {
+            int lifetimeOffset = (int)(Lifespan * UnityEngine.Random.value);
+            VariableSliceData newDatum = new VariableSliceData() { LifeRemaining = lifetimeOffset };
+            data[i] = newDatum;
+        }
+        ret.SetData(data);
+        return ret;
+    }
+
+    private ComputeBuffer GetFixedDataBuffer()
+    {
+        ComputeBuffer ret = new ComputeBuffer(PointsCount, FixedDataStride);
+        FixedSliceData[] data = new FixedSliceData[PointsCount];
+        for (int i = 0; i < PointsCount; i++)
+        {
+            Vector2 startLocation = new Vector2(UnityEngine.Random.value, UnityEngine.Random.value);
+            FixedSliceData newDatum = new FixedSliceData() { StartLocation = startLocation };
+            data[i] = newDatum;
+        }
+        ret.SetData(data);
+        return ret;
+    }
+
+    private void Update()
+    {
+        Computer.SetInt("_Lifespan", Lifespan);
+        Computer.SetFloat("_Dampener", Dampener);
+        Computer.SetTexture(_computeKernel, "_SourceTexture", NoiseSource);
+        Computer.SetBuffer(_computeKernel, "_FixedDataBuffer", _fixedDataBuffer);
+        Computer.SetBuffer(_computeKernel, "_VariableDataBuffer", _variableDataBuffer);
+        Computer.Dispatch(_computeKernel, _groupsCount, 1, 1);
+
+        Mat.SetBuffer("_FixedDataBuffer", _fixedDataBuffer);
+        Mat.SetBuffer("_VariableDataBuffer", _variableDataBuffer);
+    }
+
+    private void OnRenderObject()
+    {
+        Mat.SetPass(0);
+        Graphics.DrawProcedural(MeshTopology.Points, 1, PointsCount);
+    }
+
+    private void OnDestroy()
+    {
+        _variableDataBuffer.Release();
+        _fixedDataBuffer.Release();
+    }
+}
---
+++ Assets/SliceShader.shader
@@ -0,0 +1,89 @@
+﻿Shader "Unlit/SliceShader"
+{
+	Properties
+	{
+        _Size("Size", Float) = 1
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma geometry geo
+			#pragma fragment frag
+			
+            struct FixedSliceData 
+            {
+                float2 StartLocation;
+            };
+
+            struct VariableSliceData
+            {
+                float2 CurrentLocation;
+                float Velocity; 
+                int LifeRemaining;
+            };
+
+            StructuredBuffer<FixedSliceData> _FixedDataBuffer;
+            StructuredBuffer<VariableSliceData> _VariableDataBuffer;
+
+			struct v2g
+			{
+                float4 pos : SV_POSITION;
+            };
+
+			struct g2f
+			{
+				float4 vertex : SV_POSITION;
+			};
+            
+            v2g vert(uint meshId : SV_VertexID, uint instanceId : SV_InstanceID)
+            {
+                VariableSliceData variableData = _VariableDataBuffer[instanceId];
+
+				v2g o;
+                o.pos = float4(variableData.CurrentLocation.x,0,variableData.CurrentLocation.y, 1);
+				return o;
+			}
+
+            float _Size;
+            
+			[maxvertexcount(4)]
+			void geo(point v2g p[1], inout TriangleStream<g2f> triStream)
+			{
+				float4 vertBase = p[0].pos;
+				float4 vertBaseClip = UnityObjectToClipPos(vertBase);
+
+				float4 leftScreenOffset = float4(_Size, 0, 0, 0);
+				float4 rightScreenOffset = float4(-_Size, 0, 0, 0);
+				float4 topScreenOffset = float4(0, -_Size, 0, 0);
+				float4 bottomScreenOffset = float4(0, _Size, 0, 0); 
+
+				float4 topVertA = leftScreenOffset + topScreenOffset + vertBaseClip;
+				float4 topVertB = rightScreenOffset + topScreenOffset + vertBaseClip;
+				float4 bottomVertA = leftScreenOffset + bottomScreenOffset + vertBaseClip;
+				float4 bottomVertB = rightScreenOffset + bottomScreenOffset + vertBaseClip;
+
+				g2f o;
+				o.vertex = topVertB;
+				triStream.Append(o);
+				o.vertex = topVertA;
+				triStream.Append(o);
+				o.vertex = bottomVertB;
+				triStream.Append(o);
+				o.vertex = bottomVertA;
+				triStream.Append(o);
+            }
+			
+			fixed4 frag (g2f i) : SV_Target
+			{
+                return 1;
+			}
+			ENDCG
+		}
+	}
+}
